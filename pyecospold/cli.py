36322477f1ad65293abf8fdac1f0bf04183d3ba6
sami-m-g <sami.mg@outlook.com>
2022-12-21 18:09:45 +0200

    Moved cli from core.py to cli.py

--- pyecospold/cli.py
+++ pyecospold/cli.py
@@ -1,12 +1,39 @@
 """Command-line interface."""
+import configparser
+
 import click
 
+from .config import Defaults
+from .core import parse_file
 
-@click.command()
+
+@click.group()
 @click.version_option()
 def run() -> None:
-    """pyecospold."""
+    """pyecospold: convert ecospold XML formats to their Python equivalents and
+    export the same data back to XML."""
     pass
+
+
+@click.command("parse")
+@click.argument("input_file", type=click.Path())
+@click.option(
+    "-c",
+    "--config_file",
+    default="config.ini",
+    help="-cPath to the ini config file.",
+    type=click.Path()
+)
+def parse(input_file, config_file):
+    """ input_file: Path to the xml input file. """
+    config = configparser.ConfigParser()
+    config.read(config_file)
+    defaults = dict(config["defaults"])
+    Defaults.set_defaults(defaults)
+    parse_file(input_file)
+
+
+run.add_command(parse)
 
 
 if __name__ == "__main__":
--- pyecospold/core.py
+++ pyecospold/core.py
@@ -1,9 +1,5 @@
-import argparse
-import configparser
-
 from lxml import etree, objectify
 
-from .config import Defaults
 from .model import (MetaInformation, ProcessInformation, ReferenceFunction,
                     Geography, Technology, DataSetInformation, Validation,
                     Source, DataGeneratorAndPublication, Representativeness,
@@ -36,23 +32,3 @@ def parse_file(path: str) -> ProcessInformation:
     parser = objectify.makeparser()
     parser.set_element_class_lookup(EcospoldLookup())
     return objectify.parse(path, parser).getroot()
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-i", "--input_file", help="Path to the xml input file.",
-        type=str, required=True
-    )
-    parser.add_argument(
-        "-c", "--config_file", help="Path to the ini config file.",
-        type=str, default="config.ini"
-    )
-    pargs = parser.parse_args()
-
-    config = configparser.ConfigParser()
-    config.read(pargs.config_file)
-    defaults = dict(config["defaults"])
-    Defaults.set_defaults(defaults)
-
-    parse_file(pargs.input_file)
