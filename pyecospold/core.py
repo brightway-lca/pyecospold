0df5a65a97aaf1d9225be5bb8fb567b061d357be
sami-m-g <sami.mg@outlook.com>
2023-01-05 13:30:25 +0200

    pyecospold/core.py: added docstring

--- pyecospold/core.py
+++ pyecospold/core.py
@@ -1,3 +1,4 @@
+"""Core Ecospold module containing parsing and saving functionalities."""
 from lxml import etree, objectify
 
 from .model import (EcoSpold, Dataset, MetaInformation, FlowData,
@@ -10,7 +11,10 @@ from .model import (EcoSpold, Dataset, MetaInformation, FlowData,
 
 
 class EcospoldLookup(etree.CustomElementClassLookup):
+    """Custom XML lookup class for Ecospold files."""
+
     def lookup(self, node_type, document, namespace, name):
+        """Maps Ecospold XML elements to custom Ecospold classes."""
         lookupmap = {
             "ecoSpold": EcoSpold,
             "dataset": Dataset,
@@ -42,12 +46,26 @@ class EcospoldLookup(etree.CustomElementClassLookup):
 def parse_file(
     file_path: str, schema_path: str = "data/schema/EcoSpold01Dataset.xsd"
 ) -> EcoSpold:
+    """Parses an Ecospold XML file to custom Ecospold classes.
+
+    Parameters:
+    file_path: the path to the Ecospold XML file.
+    schema_path: the path to the Ecospold XSD schema file.
+
+    Returns and EcoSpold class representing the root of the XML file.
+    """
     schema = etree.XMLSchema(file=schema_path)
     parser = objectify.makeparser(schema=schema)
     parser.set_element_class_lookup(EcospoldLookup())
     return objectify.parse(file_path, parser).getroot()
 
 
-def save_file(root: etree.ElementBase, path: str) -> None:
+def save_file(root: EcoSpold, path: str) -> None:
+    """Saves an Ecospold class to an XML file.
+
+    Parameters:
+    root: the EcoSpold class representing the root of the XML file.
+    path: the path to save the Ecospold XML file.
+    """
     root = etree.ElementTree(root)
     root.write(path, pretty_print=True, xml_declaration=True, encoding="UTF-8")
