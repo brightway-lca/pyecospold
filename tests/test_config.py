c3c47997f53d7f314012b7b1ca2568693e210c5e
sami-m-g <sami.mg@outlook.com>
2023-01-15 19:15:45 +0200

    tests/test_config.py: added test_config_defaults

--- tests/test_config.py
+++ tests/test_config.py
@@ -3,6 +3,14 @@
 from pyecospold.config import Defaults
 
 
+def test_config_defaults() -> None:
+    """It overrides defaults variables."""
+    configFile = "config.ini"
+    Defaults.config_defaults(configFile)
+
+    assert Defaults.qualityNetwork == "1"
+
+
 def test_set_defaults() -> None:
     """It overrides defaults variables."""
     defaults = {"qualityNetwork": "2"}
