2bd5aeab8f6eacda7acc08b4dd0bab5b37bc404b
sami-m-g <sami.mg@outlook.com>
2023-01-16 14:34:04 +0200

    tests/test_core.py: fixed test_parse_directory_v1 failures on macos

--- tests/test_core.py
+++ tests/test_core.py
@@ -10,11 +10,12 @@ from pyecospold.core import parse_directory_v1, parse_file_v1, save_file
 def test_parse_directory_v1() -> None:
     """It reads all files successfully."""
     dirPath = os.path.join(Path(__file__).parent.parent.resolve(), "data", "v1")
-    ecospoldList = parse_directory_v1(dirPath)
+    files = [os.path.join(dirPath, "v1_1.xml"), os.path.join(dirPath, "v1_2.spold")]
+    ecospoldList = sorted(parse_directory_v1(dirPath))
 
     assert len(ecospoldList) == 2
-    assert ecospoldList[0][0] == Path(os.path.join(dirPath, "v1_1.xml"))
-    assert ecospoldList[1][0] == Path(os.path.join(dirPath, "v1_2.spold"))
+    assert ecospoldList[0][0] == Path(files[0])
+    assert ecospoldList[1][0] == Path(files[1])
     assert ecospoldList[0][1].dataset.generator == "EcoAdmin 1.1.17.110"
     assert ecospoldList[1][1].dataset.generator == "EcoAdmin 1.1.17.110"
 
