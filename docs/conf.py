ebcfe029f2621d44f375d131e0d5ac8ce7385939
sami-m-g <sami.mg@outlook.com>
2024-04-03 15:03:13 +0200

    docs/conf.py: fixed importlib.metadata import

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 import datetime
-import importlib
+import importlib.metadata
 
 project = 'pyecospold'
 copyright = f'{datetime.date.today().year}, Mina Sami'
